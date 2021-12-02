bdecc0609a8ed07fccbdbea6731d8083da5e82d2
kyawk <kyawk@DESKTOP-JI8827H>
2021-12-03 05:17:09 +0630

    Add project files.

---
+++ Flasher/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Flasher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.fw = new System.Windows.Forms.Button();
+            this.fl = new System.Windows.Forms.Button();
+            this.inf = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(7, 45);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(321, 412);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(7, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(321, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.tb1_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Firmware";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.Black;
+            this.richTextBox2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(334, 46);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(288, 411);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // fw
+            // 
+            this.fw.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fw.Location = new System.Drawing.Point(334, 21);
+            this.fw.Name = "fw";
+            this.fw.Size = new System.Drawing.Size(93, 23);
+            this.fw.TabIndex = 5;
+            this.fw.Text = "button1";
+            this.fw.UseVisualStyleBackColor = true;
+            this.fw.Click += new System.EventHandler(this.fw_Click);
+            // 
+            // fl
+            // 
+            this.fl.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fl.Location = new System.Drawing.Point(431, 20);
+            this.fl.Name = "fl";
+            this.fl.Size = new System.Drawing.Size(93, 23);
+            this.fl.TabIndex = 6;
+            this.fl.Text = "button1";
+            this.fl.UseVisualStyleBackColor = true;
+            this.fl.Click += new System.EventHandler(this.fl_Click);
+            // 
+            // inf
+            // 
+            this.inf.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inf.Location = new System.Drawing.Point(529, 20);
+            this.inf.Name = "inf";
+            this.inf.Size = new System.Drawing.Size(93, 23);
+            this.inf.TabIndex = 7;
+            this.inf.Text = "button1";
+            this.inf.UseVisualStyleBackColor = true;
+            this.inf.Click += new System.EventHandler(this.inf_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(150, 461);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Select All";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(242, 461);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Unselect All";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(7, 465);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(134, 18);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Disable AVB(MTK)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(334, 464);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(158, 18);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "Device With Dual Slot";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBox1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Activate Slot A",
+            "Activate Slot B"});
+            this.comboBox1.Location = new System.Drawing.Point(494, 461);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(128, 22);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 491);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inf);
+            this.Controls.Add(this.fl);
+            this.Controls.Add(this.fw);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button fw;
+        private System.Windows.Forms.Button fl;
+        private System.Windows.Forms.Button inf;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Flasher/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using RegawMOD.Android;
+
+namespace Flasher
+{
+    public partial class Form1 : Form
+    {
+        public string args;
+        AndroidController android = AndroidController.Instance;
+        bool blu;
+        string pdn;
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            this.Text = "Fastboot Flasher";
+            fw.Text = "Select";
+            fl.Text = "Flash";
+            inf.Text = "Info";
+            textBox1.Text = "Double click to load firmware";
+            comboBox1.Enabled = false;
+            checkBox2.Checked = false;
+            checkBox1.Checked = false;            
+            richTextBox2.AppendText("Developed By : ",Color.Yellow);
+            richTextBox2.AppendText("Kyaw Khant Zaw",Color.LimeGreen);
+        }
+
+        private void ParseFlashBat()
+        {
+            richTextBox2.Clear();
+            checkedListBox1.Items.Clear();
+            using (StreamReader r = new StreamReader(textBox1.Text+"\\flash_all.bat"))
+            {
+                string line;
+                int i = 0;
+                while ((line = r.ReadLine()) != null)
+                {
+                    if(line.Contains("flash"))
+                    {                       
+                        string[] ls = line.Split('|');
+                        string sf=ls[0].Replace(" %* "," ");
+                        string fs = sf.Replace(" %~dp0"," ");
+                        string l = fs.Replace("fastboot ", " ");
+                        string fn = l.TrimStart();
+                        fn = fn.Replace("images","=");
+                        if (fn.Contains(":"))
+                        {
+                            fn = fn.Replace(":", " ");
+                            fn = fn.TrimStart();
+                        }
+                        if (fn.Contains("\\"))
+                        {
+                            fn = fn.Replace("\\", " ");
+                        }
+                        string[] final = fn.Split('=');
+                        string cmd = final[0].TrimStart().TrimEnd()+" \\images\\"+final[1].TrimStart().TrimEnd();                                           
+                        checkedListBox1.Items.Add(cmd);
+                        checkedListBox1.SetItemChecked(i, true);
+                        i++;
+                    }                    
+                }
+            }
+        }
+
+        private void FBInfo()
+        {
+            android.UpdateDeviceList();
+            if (android.HasConnectedDevices)
+            {
+                var device = android.GetConnectedDevice(android.ConnectedDevices[0]);
+                richTextBox2.AppendText(device.State.ToString(),Color.Yellow);
+                switch (device.State.ToString())
+                {
+                    case "FASTBOOT":
+                        if (File.Exists(Directory.GetCurrentDirectory() + "\\f.txt"))
+                        {
+                            File.Delete(Directory.GetCurrentDirectory() + "\\f.txt");
+                        }
+                        File.WriteAllText(Directory.GetCurrentDirectory() + "\\fb.cmd", Properties.Resources.fb);                        
+                        Process fd=new Process();
+                        fd.StartInfo.FileName = "cmd.exe";
+                        fd.StartInfo.Arguments = "/c fb.cmd 2> f.txt";
+                        fd.StartInfo.UseShellExecute = false;
+                        fd.StartInfo.CreateNoWindow = true;
+                        fd.StartInfo.WorkingDirectory = Directory.GetCurrentDirectory();
+                        fd.StartInfo.RedirectStandardOutput = true;
+                        fd.Start();
+                        fd.WaitForExit();                                               
+                        File.Delete(Directory.GetCurrentDirectory() + "\\fb.cmd");
+                        using (StreamReader r = new StreamReader(Directory.GetCurrentDirectory() + "\\f.txt"))
+                        {
+                            string line;
+                            int i = 0;
+                            while ((line = r.ReadLine()) != null)
+                            {                                
+                                if (line.Contains("unlocked"))
+                                {
+                                    line = line.Replace("(bootloader) ", " ").TrimStart();
+                                    string[] pd = line.Split(':');
+                                    richTextBox2.AppendText(Environment.NewLine+"Unlocked : ", Color.Yellow);
+                                    if (pd[1].Contains("yes"))
+                                    {
+                                        richTextBox2.AppendText(pd[1].TrimStart().TrimEnd().ToUpper(), Color.LimeGreen);
+                                        blu = true;
+                                    }
+                                    else
+                                    {
+                                        richTextBox2.AppendText(pd[1].TrimStart().TrimEnd(), Color.Red);
+                                        blu = false;
+                                    }
+                                }
+                                if (line.Contains("product"))
+                                {
+                                    line = line.Replace("(bootloader) ", " ").TrimStart();
+                                    string[] pd = line.Split(':');
+                                    richTextBox2.AppendText(Environment.NewLine + "Product : ", Color.Yellow);
+                                    richTextBox2.AppendText(pd[1].TrimStart().TrimEnd(), Color.LimeGreen);
+                                    pdn = pd[1].TrimStart().TrimEnd();
+                                }
+                                if (line.Contains("slot-count"))
+                                {
+                                    line = line.Replace("(bootloader) ", " ").TrimStart();
+                                    string[] pd = line.Split(':');
+                                    richTextBox2.AppendText(Environment.NewLine + pd[0]+" : ", Color.Yellow);
+                                    richTextBox2.AppendText(pd[1].TrimStart().TrimEnd() + Environment.NewLine, Color.LimeGreen);
+                                    pdn = pd[1].TrimStart().TrimEnd();
+                                }
+                                i++;                               
+                            }                            
+                        }
+                        richTextBox2.AppendText(Environment.NewLine);
+                        break;
+                    default:
+                        richTextBox2.AppendText("Device not in fastboot.",Color.Red);
+                        break;
+                }
+                android.Dispose();
+            }
+            else
+            {
+                richTextBox2.AppendText("No Device Found!", Color.Red);
+                android.Dispose();
+            }
+        }
+
+        private void FBFlash()
+        {
+            this.Cursor = Cursors.WaitCursor;
+            int i = 0;
+            while (i < checkedListBox1.Items.Count)
+            {                
+                if(checkedListBox1.GetItemChecked(i) == true)
+                {
+                    args = checkedListBox1.Items[i].ToString();
+                    string []argsp=args.Split('\\');                    
+                    string log = argsp[0].Replace("flash ", " ");
+                    richTextBox2.AppendText(Environment.NewLine+"Flashing "+log.TrimStart().TrimEnd()+"...",Color.Yellow);
+                    Fastboot.ExecuteFastbootCommandNoReturn(Fastboot.FormFastbootCommand(argsp[0],"\""+textBox1.Text+"\\"+argsp[1]+"\\"+argsp[2]+"\""));                 
+                    richTextBox2.AppendText("Done",Color.LimeGreen);                    
+                    richTextBox2.ScrollToCaret();
+                }
+                i++;
+            }
+            if (checkBox1.Checked == true)
+            {
+                richTextBox2.AppendText(Environment.NewLine + "Disabling Verified Boot...",Color.Yellow);
+                Fastboot.ExecuteFastbootCommandNoReturn(Fastboot.FormFastbootCommand("--disable-verity --disable-verification flash vbmeta","\""+ textBox1.Text + "\\images\\vbmeta.img"+"\""));
+                richTextBox2.AppendText("Done",Color.LimeGreen);
+            }
+            if (checkBox2.Checked == true)
+            {
+                richTextBox2.AppendText(Environment.NewLine + "Setting Active to " + comboBox1.SelectedItem.ToString(), Color.Yellow);
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        Fastboot.ExecuteFastbootCommandNoReturn(Fastboot.FormFastbootCommand("set_active a"));                        
+                        break;
+                        case 1:
+                        Fastboot.ExecuteFastbootCommandNoReturn(Fastboot.FormFastbootCommand("set_active b"));
+                        break;
+                }
+                richTextBox2.AppendText("Done", Color.Green);
+            }
+            richTextBox2.AppendText(Environment.NewLine+"Developed By :",Color.Yellow);
+            richTextBox2.AppendText("Kyaw Khant Zaw", Color.LimeGreen);
+            this.Cursor = Cursors.Default;            
+        }
+
+        private void fwbrose()
+        {
+            var f = new FolderBrowserDialog();
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                string p = f.SelectedPath;
+                if (!File.Exists(p + "\\flash_all.bat") && (!Directory.Exists(p + "\\images")))
+                {
+                    MessageBox.Show("Flash script not found!");
+                }
+                else
+                {
+                    textBox1.Text = p;
+                    ParseFlashBat();
+                }
+            }
+        }
+        private void fw_Click(object sender, EventArgs e)
+        {
+            fwbrose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (i<checkedListBox1.Items.Count)
+            {
+                checkedListBox1.SetItemChecked(i, false);
+                i++;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (i < checkedListBox1.Items.Count)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+                i++;
+            }
+        }
+
+        private void fl_Click(object sender, EventArgs e)
+        {
+            if(textBox1.TextLength>5 && checkedListBox1.Items.Count>1 && !textBox1.Text.Contains("Double"))
+            {                
+                richTextBox2.Clear();
+                android.UpdateDeviceList();
+                if (android.HasConnectedDevices)
+                {
+                    var device = android.GetConnectedDevice(android.ConnectedDevices[0]);
+                    switch (device.State.ToString())
+                    {
+                        case "FASTBOOT":
+                            FBInfo();
+                            if (blu)
+                            {
+                                this.Text = "Fastboot Flasher (Flashing : Don't touch me)";
+                                FBFlash();
+                                DialogResult d = MessageBox.Show("Operation Done!" + Environment.NewLine + "Do you want to reboot?", "Alert", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+                                if (d == DialogResult.Yes)
+                                {
+                                    device.FastbootReboot();
+                                }
+                                this.Text = "Fastboot Flasher";
+                            }
+                            else
+                            {
+                                richTextBox2.AppendText(Environment.NewLine + "Bootloader is locked. Aboaring!", Color.Red);
+                            }
+                            break;
+                        default:
+                            MessageBox.Show("Device not in fastboot.");
+                            break;
+                    }
+                    android.Dispose();
+                }
+                else
+                {
+                    richTextBox2.AppendText("No Device Found!", Color.Red);
+                    android.Dispose();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Select firmware first", "Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }           
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                comboBox1.Enabled = true;
+                comboBox1.SelectedIndex = 0;
+            }
+            else
+            {
+                comboBox1.Enabled = false;
+            }
+        }
+
+        private void inf_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+            FBInfo();
+        }
+
+        private void tb1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            fwbrose();
+        }
+    }
+    public static class RichTextBoxExtension
+    {
+        public static void AppendText(this RichTextBox box, string text, Color color)
+        {
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+            box.SelectionColor = color;
+            box.AppendText(text);
+            box.SelectionColor = box.ForeColor;
+        }
+    }
+}
